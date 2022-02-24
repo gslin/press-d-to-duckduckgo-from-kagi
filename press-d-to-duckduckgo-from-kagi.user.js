b6704b16ac7d259f605e63f0f429f7d3cd403fa4
Gea-Suan Lin <gslin@gslin.org>
2022-02-24 17:11:23 +0800

    Import.

---
+++ press-d-to-duckduckgo-from-kagi.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        Press "d" to DuckDuckGo from Kagi
+// @namespace   https://github.com/gslin/press-d-to-duckduckgo-from-kagi
+// @match       https://kagi.com/search*
+// @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_registerMenuCommand
+// @grant       GM_setValue
+// @version     0.20220224.0
+// @author      Gea-Suan Lin <gslin@gslin.com>
+// @description Press "d" to DuckDuckGo from Kagi.
+// @require     https://greasyfork.org/scripts/38445-monkeyconfig/code/MonkeyConfig.js?version=251319
+// @license     MIT
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    const cfg = new MonkeyConfig({
+        menuCommand: true,
+        params: {
+            search_engine: {
+                type: 'text',
+                default: 'https://www.duckduckgo.com/?q=',
+            },
+        },
+        title: 'Press "d" to DuckDuckGo in Kagi',
+    });
+
+    window.addEventListener('keyup', function(event) {
+        if ('input' === document.activeElement.tagName.toLowerCase()) {
+            return;
+        }
+        if ('d' !== event.key) {
+            return;
+        }
+
+        let q = document.getElementById('searchBar').value;
+        let q_encoded = encodeURIComponent(q).replace(/%20/g, '+');
+        let url = cfg.get('search_engine') + q_encoded;
+
+        document.location = url;
+    });
+})();
